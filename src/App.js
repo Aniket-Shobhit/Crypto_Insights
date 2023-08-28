37a46876a5e70467e85a7d451e3346f2610e3789
Aniket-Shobhit <shobhitaniket3@gmail.com>
2023-08-29 01:48:42 +0530

    navbar & basis layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
+import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space} from 'antd';
+import Navbar from './components/Navbar';
+import Homepage from './components/Homepage';
+import News from './components/News';
+import Cryptocurrencies from './components/Cryptocurrencies';
+import CryptoDetails from './components/CryptoDetails';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className='app'>
+      <div className='navbar'>
+        <Navbar />
+      </div>
+      <div className='main'>
+        <Layout>
+          <div className='routes'>
+            <Switch>
+              <Route exact path='/'>
+                <Homepage />
+              </Route>
+              <Route exact path='/cryptocurrencies'>
+                <Cryptocurrencies/>
+              </Route>
+              <Route exact path='/crypto/:coinId'>
+                <CryptoDetails />
+              </Route>
+              <Route exact path='/news'>
+                <News />
+              </Route>
+            </Switch>
+          </div>
+        </Layout>
+      </div>
+      <div className='footer'>
+        
+      </div>
     </div>
   );
 }
